0c3d4c8b2e5e31874f441faf5db513ae6b527731
Rommel García Hernández <rommelgh@gmail.com>
2018-10-31 14:30:51 -0600

    Update buffer_overflow.c

--- buffer_overflow.c
+++ buffer_overflow.c
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
     if (argc != 2) {
         printf("Please supply a string as an argument!\n");
         return -1;
-	} 
+    } 
     my_process(argv[1]);
     return 0;
 }
