b01d078914e117e5ff4f3ed0eea77304714a0e0e
Rommel García Hernández <rommelgh@gmail.com>
2018-10-31 14:37:08 -0600

    Update integer_overflow.c

--- integer_overflow.c
+++ integer_overflow.c
@@ -5,10 +5,10 @@ int main(int argc, char *argv[]) {
     char buf[80];
     int i = atoi(argv[1]);
     unsigned short s = i;
-	if (s >= 80) {
-	    printf("Check failed\n");
-	    return 0;
-	}
+    if (s >= 80) {
+        printf("Check failed\n");
+        return 0;
+    }
     printf("Check passed\n");
     memcpy(buf, argv[2], i);
     return 0;
